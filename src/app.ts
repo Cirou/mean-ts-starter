ce5ac771bcf97145e5064359e5f5327c68289565
Cirou <ciroaiello@live.com>
2018-02-12 12:27:30 +0100

    First commit

---
+++ src/app.ts
@@ -0,0 +1,94 @@
+import * as express from "express";
+import * as compression from "compression";  // compresses requests
+import * as session from "express-session";
+import * as bodyParser from "body-parser";
+import * as logger from "morgan";
+import * as lusca from "lusca";
+import * as dotenv from "dotenv";
+import * as mongo from "connect-mongo";
+import * as flash from "express-flash";
+import * as path from "path";
+import * as mongoose from "mongoose";
+import * as passport from "passport";
+import * as expressValidator from "express-validator";
+import * as bluebird from "bluebird";
+
+const MongoStore = mongo(session);
+
+// Load environment variables from .env file
+dotenv.config({ path: "dev.env.example" });
+
+// Controllers (route handlers)
+import * as apiController from "./controllers/api";
+
+// Create Express server
+const app = express();
+const rootPath = path.normalize(__dirname + "/../");
+
+// Connect to MongoDB
+const mongoUrl = process.env.MONGOLAB_URI;
+(<any>mongoose).Promise = bluebird;
+mongoose.connect(mongoUrl, {useMongoClient: true}).then(
+  () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+).catch(err => {
+  console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+  // process.exit();
+});
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
+app.use(compression());
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET,
+  store: new MongoStore({
+    url: mongoUrl,
+    autoReconnect: true
+  })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(lusca.xframe("SAMEORIGIN"));
+app.use(lusca.xssProtection(true));
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+app.use((req, res, next) => {
+  // After successful login, redirect back to the intended page
+  if (!req.user &&
+    req.path !== "/login" &&
+    req.path !== "/signup" &&
+    !req.path.match(/^\/auth/) &&
+    !req.path.match(/\./)) {
+    req.session.returnTo = req.path;
+  } else if (req.user &&
+    req.path === "/account") {
+    req.session.returnTo = req.path;
+  }
+  next();
+});
+app.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 }));
+
+/**
+ * API routes.
+ */
+app.get("/api", apiController.getApi);
+
+/**
+ * Primary app routes for Angular will catch all route
+ * Keep this one as the last one
+ */
+app.get("*", function(req, res) {
+  res.sendFile(rootPath + "dist/public/index.html", { user: req.user });
+});
+
+module.exports = app;
